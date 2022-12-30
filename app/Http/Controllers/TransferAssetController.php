f8856746e847c1c55bb96f1f78943e61fc74c73c
vaishaksuvarna <vaishakkpoojary@gmail.com>
2022-12-30 12:56:27 +0530

    minor changes in code

--- app/Http/Controllers/TransferAssetController.php
+++ app/Http/Controllers/TransferAssetController.php
@@ -6,9 +6,42 @@ use Illuminate\Http\Request;
 use App\Models\Asset;
 use Exception;
 use Illuminate\Database\QueryException;
+use Str;
+use Storage;
 
 class TransferAssetController extends Controller
 {
+
+    public function getExtension($image)
+    {
+        $extension = explode('/', explode(':', substr($image, 0, strpos($image, ';')))[1])[1];
+        if ($extension == 'jpg') {
+            $response = 'jpg';
+
+        } elseif ($extension == 'png') {
+            $response = 'png';
+
+        } elseif ($extension == 'jpeg') {
+            $response = 'jpeg';
+            
+        } elseif ($extension == 'csv') {
+            $response = 'csv';
+
+        } else {
+
+            $extension1 = explode(";", $image)[0];
+            $x = explode(".", $extension1)[3];
+            if ($x == 'document') {
+                $response = "docx";
+            } elseif ($x == 'sheet') {
+                $response = "xlsx";
+            } else {
+                $response = $extension;
+            }
+        }
+
+        return $response;
+    }
 
     public function transferData(Request $request, $id)
     {
@@ -27,12 +60,26 @@ class TransferAssetController extends Controller
                 }else{
                     $asset->transfer = 0;
                 }
+                $asset->unit = $request->unit;
+                $asset->project = $request->project;
                 $asset->department = $request->department;
                 $asset->section = $request->section;
-                $asset->assetType = $request->assetType;
+                $asset->line = $request->line;
+                $asset->remarks = $request->remarks;
+
+                $image = $request->fileUpload;
+                if($image){  // your base64 encoded
+                    $extension = $this->getExtension($image);
+                    $replace = substr($image, 0, strpos($image, ',')+1); 
+                    $image = str_replace($replace, '', $image); 
+                    $image = str_replace(' ', '+', $image); 
+                    $imageName = Str::random(10).'.'.$extension;
+                    $imagePath = '/storage/app/public/'.$imageName;
+                    Storage::disk('public')->put($imageName, base64_decode($image));
+                    $asset->fileUpload = $imagePath;
+                }
             
                 $asset->save();
-              
                 $response = [
                     "massage" => "transfered successfully",
                     "status" => 200
